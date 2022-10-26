b9b69464f34c5e4842c325271f29a03d77f21710
Roman Grigoriev <108183544+grigoriev-rk@users.noreply.github.com>
2022-10-26 12:46:39 +0300

    Update HW_dec.py

--- HW_dec.py
+++ HW_dec.py
@@ -1,18 +1,24 @@
 import datetime
 
-def decorator(func):           #Decorator that return result of all interrior functions
-    def record(args,**kwargs):      #Datatime part of code that return into decorator required data
-        with open('1.txt', 'w', encoding = 'utf-8') as f:
-            date = datetime.datetime.now().strftime("%b %d %Y %H:%M:%S")
-            result = func(*args, **kwargs)
-            f.write(date)
-            f.write(f'Имя: {func}, Аргументы: {args} , {kwargs}')
-            f.write(f'Результат: {result}')
-        return (result)
-    return (record)
+wage = 100
+bonus = 50 
 
-@decorator                          #Applying decorator
-def calculate_salary():             #initial function
-    print ('calculate salary')
+def parametrized_decor(path):           #Parametrized decorator with file path
+    def decorator(func):                #Decorator that return result of all interrior functions
+        def record(args,**kwargs):      #Datatime part of code that return into decorator required data
+            with open(path, 'w', encoding = 'utf-8') as f:
+                date = datetime.datetime.now().strftime("%b %d %Y %H:%M:%S")
+                result = func(*args, **kwargs)
+                f.write(date)
+                f.write(f'Имя: {func}, Аргументы: {args} , {kwargs}')
+                f.write(f'Результат: {result}')
+            return (result)
+        return (record)
+
+@parametrized_decor(path = 'logs/1.txt')       #Applying decorator
+def calculate_salary(wage, bonus):             #initial function
+    salary = wage + bonus
+    print (f'Зарплата составляет {salary} условных единиц')
+
 
 
